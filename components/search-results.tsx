b3ac873a7c620aa0b95a35da84c0c7b790ce5258
Akhil <akhil1659@gmail.com>
2024-09-06 02:11:24 +0530

    changes in ui

--- components/search-results.tsx
+++ components/search-results.tsx
@@ -18,6 +18,17 @@ interface SearchResultsTypes{
 export default function SearchResults({
     filteredChallenges
 }: SearchResultsTypes) {
+
+    if(!filteredChallenges){
+        return (
+            <div className='w-full h-[350px] bg-[#003145] text-white'>
+                <div className='h-full w-full flex justify-center items-center font-extrabold text-xl'>
+                    No Challenges found!
+                </div>
+            </div>
+        )
+    }
+
   return (
     <div className='w-full h-full bg-[#003145]'>
        <div className='h-full w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-y-8'>
